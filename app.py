8d27544f9ad4d200d4602be0cdfa0bc58ba2768e
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-17 01:10:05 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template, jsonify
+import joblib
+import os
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+
+# Inisialisasi aplikasi Flask
+app = Flask(__name__)
+
+# Fungsi preprocess_text
+def preprocess_text(text):
+    tokens = word_tokenize(text)
+    tokens = [token.lower() for token in tokens]
+    tokens = [token for token in tokens if token.isalnum()]
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(token) for token in tokens]
+    return ' '.join(tokens)
+
+# Memuat model
+model_filename = './model/trained_model.pkl'
+pipe_svc = joblib.load(model_filename)
+
+# Route untuk halaman utama
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route untuk memprediksi email
+@app.route('/predict', methods=['POST'])
+def predict():
+    email_text = request.form['email_text']
+    email_text = preprocess_text(email_text)
+    prediction = pipe_svc.predict([email_text])
+    result = 'spam' if prediction[0] == 1 else 'ham'
+    return render_template('index.html', prediction=result)
+
+if __name__ == '__main__':
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    app.run(debug=True)
