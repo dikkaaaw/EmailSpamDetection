592f78e73aded014f5df61a980021f6abbcbf91f
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-17 17:12:51 +0700

    chore: Refactor app.py and update dependencies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template
 import joblib
 import nltk
 from nltk.tokenize import word_tokenize
