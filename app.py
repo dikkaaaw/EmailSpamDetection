e3b111a13964c23e73d59b2fa4379647bd1662f5
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-29 23:44:06 +0700

    Refactor app.py to improve text preprocessing

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import joblib
 import nltk
 import os
-from preprocess import preprocess_text  # Impor fungsi preprocess_text
+from preprocess import preprocess_text  # Pastikan ini terimpor
 
 # Inisialisasi aplikasi Flask
 app = Flask(__name__)
@@ -11,34 +11,34 @@ app = Flask(__name__)
 nltk.download('stopwords', quiet=True)
 nltk.download('punkt', quiet=True)
 
-# Memuat model
-model_filename = os.path.join(os.path.dirname(__file__), 'model', 'trained_model.pkl')
-print(f"Loading model from {model_filename}")
-
-# Definisikan preprocessing fungsi dalam konteks joblib
+# Fungsi khusus untuk memuat model dengan globals
 def custom_unpickler():
     import preprocess
-    preprocess.preprocess_text = preprocess_text
+    globals()['preprocess_text'] = preprocess.preprocess_text
 
-pipe_svc = joblib.load(model_filename)
+# Memuat model
+model_filename = os.path.join(os.path.dirname(__file__), 'model', 'trained_model.pkl')
+if os.path.exists(model_filename):
+    custom_unpickler()  # Panggil fungsi untuk memastikan preprocess_text tersedia
+    pipe_svc = joblib.load(model_filename)
+else:
+    raise FileNotFoundError(f"Model file {model_filename} not found. Please ensure the path is correct.")
 
 # Route untuk halaman utama
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict')
-def form():
-    return render_template('form.html')
-
-# Route untuk memprediksi email
 @app.route('/predict', methods=['POST'])
 def predict():
     email_text = request.form['email_text']
     email_text = preprocess_text(email_text)
     prediction = pipe_svc.predict([email_text])
     result = 'spam' if prediction[0] == 1 else 'ham'
-    return render_template('form.html', prediction=result)
+    response = {
+        'prediction_text': f'Prediksi: {result}',
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
