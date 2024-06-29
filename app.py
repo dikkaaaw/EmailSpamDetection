4745dbd1b48f9fa737177f47e745efeb94d9b3dd
Dika Wicaksono <dhkwck00@gmail.com>
2024-06-29 23:30:26 +0700

    Refactor app.py to improve text preprocessing

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 from flask import Flask, request, render_template
 import joblib
 import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
 import os
+from preprocess import preprocess_text  # Impor fungsi preprocess_text
 
 # Inisialisasi aplikasi Flask
 app = Flask(__name__)
-
-# Fungsi preprocess_text
-def preprocess_text(text):
-    tokens = word_tokenize(text)
-    tokens = [token.lower() for token in tokens]
-    tokens = [token for token in tokens if token.isalnum()]
-    stemmer = PorterStemmer()
-    tokens = [stemmer.stem(token) for token in tokens]
-    return ' '.join(tokens)
 
 # Memastikan dataset NLTK diunduh
 nltk.download('stopwords', quiet=True)
 nltk.download('punkt', quiet=True)
 
-# Memuat model dengan globals
+# Memuat model
 model_filename = os.path.join(os.path.dirname(__file__), 'model', 'trained_model.pkl')
 print(f"Loading model from {model_filename}")
-pipe_svc = joblib.load(model_filename, mmap_mode=None, globals={'preprocess_text': preprocess_text})
+
+# Definisikan preprocessing fungsi dalam konteks joblib
+def custom_unpickler():
+    import preprocess
+    preprocess.preprocess_text = preprocess_text
+
+pipe_svc = joblib.load(model_filename)
 
 # Route untuk halaman utama
 @app.route('/')
